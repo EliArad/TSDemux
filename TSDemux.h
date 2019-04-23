4529ea9d074a20891bbc760615b61f81b7938d68
Eli Arad <easp13@gmail.com>
2019-04-23 23:24:48 +0300

    initial code

---
+++ TSDemux.h
@@ -0,0 +1,33 @@
+#pragma once
+#include "bitfile.h"
+#include <iostream>
+#include <fstream>
+#include <cstring>
+#include <fstream>
+#include "TSPacket.h"
+
+using namespace std;
+
+#define SYNC_BYTE 0x47
+
+ 
+
+class TSDemux
+{
+public:
+	TSDemux();
+	~TSDemux();
+
+	void Start(const char *fileName);
+
+private:
+	void ReadInput(const char *fileName);
+	int transport_packet();
+	void adaptation_field();
+
+private:
+	bit_file_c bf;
+
+	TSPacket  m_tsp;
+};
+
